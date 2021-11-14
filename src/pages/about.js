2a0cdba7ed2a30ae55a89452d8fe29ec88778d42
SuriyaTasmimDisha <tdisha67@gmail.com>
2021-11-14 10:26:26 +0600

    done with second tutorial

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "./layout";
+
+const About = () => {
+	return (
+		<Layout pageTitle="About Me">
+			<p>What do you wanna know??</p>
+		</Layout>
+	);
+};
+
+export default About;
