4dd9a407c7358e91ecdbedc4a9273d7e60bc99f0
SuriyaTasmimDisha <tdisha67@gmail.com>
2021-11-14 11:14:27 +0600

    done with third tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { StaticImage } from "gatsby-plugin-image";
 import * as React from "react";
 import Layout from "./layout";
 
@@ -6,6 +7,10 @@ const IndexPage = () => {
 	return (
 		<Layout pageTitle="Home Page">
 			<p>Welcome to my blog</p>
+			<StaticImage
+				src="../images/dog and cat friend.png"
+				alt="dog and cat friend"
+			/>
 		</Layout>
 	);
 };
